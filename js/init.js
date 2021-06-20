499453ae4a36f9eb5673e86e1f6bd08f1bd27e2a
kasachiefs2 <86207377+kasachiefs2@users.noreply.github.com>
2021-06-20 18:36:38 -0500

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,10 @@
+firebase.initializeApp({
+apiKey: "AIzaSyCNAbE-JW8W0hHokizGKelC_S6BShxMKUM",
+    authDomain: "examen-tercer-departamental.firebaseapp.com",
+    projectId: "examen-tercer-departamental",
+    storageBucket: "examen-tercer-departamental.appspot.com",
+    messagingSenderId: "782121819836",
+    appId: "1:782121819836:web:3ce4acf3fe7ff90a047ab9",
+    measurementId: "G-N7T22SLD50"
+  });
+
